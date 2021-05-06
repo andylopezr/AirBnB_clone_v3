7ce91eb1a5b5c1c693f8f39c30c76589616d2ed6
Andres Lopez <profesor.de.ingles@aol.com>
2021-05-06 22:32:42 +0000

    ADDED | endpoint to retrieve object type count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,24 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status')
 def status():
     """Returns a JSON from API route"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/status')
+def stats():
+    """Returns number of objects by type"""
+    stats = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
+    for obj, name in stats.items()
+        return jsonify({name: storage.count(obj)})
