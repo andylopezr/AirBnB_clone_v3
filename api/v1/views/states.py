f485c9fd371fe0ea8cd3a439c0b2535091b1a1e9
ANDRESOTELO <andresotelo2020@gmail.com>
2021-05-07 20:07:55 +0000

    ADDED | Delete, Put and Post method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""View for State objects"""
 
 from models import storage
 from models.state import State
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 from api.v1.views import app_views
 
 state = 'State'
@@ -23,7 +24,7 @@ def one_state(state_id=None):
         key_split = key.split(".")
         if state_id == key_split[1]:
             return jsonify(value.to_dict())
-    return jsonify({"error": "Not found"}), 404
+    abort(404)
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'],
@@ -36,4 +37,46 @@ def del_states(state_id):
         obj_state.delete()
         storage.save()
         return({})
-    return jsonify({"error": "Not found"}), 404
+    abort(404)
+
+
+@app_views.route('/states', methods=['POST'],
+                 strict_slashes=False)
+def post_states():
+    """Post state based on json"""
+    # transform the HTTP body request to a dictionary
+    obj_dict = request.get_json()
+    if obj_dict is None:
+        abort(400, 'Not a JSON')
+    if 'name' in obj_dict.keys():
+        obj_state = State(**obj_dict)
+        obj_state.save()
+        return jsonify(obj_state.to_dict()), 201
+    else:
+        abort(400, 'Missing name')
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_states(state_id):
+    """Update state based on state_id"""
+    obj_state = storage.get(State, state_id)
+
+    # These keys cannot be update
+    ignore_keys = ['id', 'created_at', 'updated_at']
+
+    # transform the HTTP body request to a dictionary
+    to_update = request.get_json()
+    if to_update is None:
+        abort(400, 'Not a JSON')
+    # check if key in dictionary is not allowed to be updated
+    for key_ignore in ignore_keys:
+        if key_ignore in to_update.keys():
+            del to_update[key_ignore]
+    if obj_state:
+        for key, value in to_update.items():
+            setattr(obj_state, key, value)
+        obj_state.save()
+        return jsonify(obj_state.to_dict())
+    else:
+        abort(404)
