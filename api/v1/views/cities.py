a9e870f4abb61b405385bdbbdf9a0a5db9a75e32
Andres Lopez <profesor.de.ingles@aol.com>
2021-05-08 00:04:18 +0000

    ADDED | put and post methods

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -45,3 +45,45 @@ def del_city(city_id):
         storage.save()
         return({})
     abort(404)
+
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
+def post_city():
+    """Post city based on json"""
+    # transform the HTTP body request to a dictionary
+    obj_dict = request.get_json()
+    if obj_dict is None:
+        abort(400, 'Not a JSON')
+    if 'name' in obj_dict.keys():
+        obj_city = City(**obj_dict)
+        obj_city.save()
+        return jsonify(obj_city.to_dict()), 201
+    else:
+        abort(400, 'Missing name')
+
+
+@app_views.route('/cities/<city_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_states(city_id):
+    """Update state based on state_id"""
+    obj_city = storage.get(City, city_id)
+
+    # These keys cannot be update
+    ignore_keys = ['id', 'created_at', 'updated_at']
+
+    # transform the HTTP body request to a dictionary
+    to_update = request.get_json()
+    if to_update is None:
+        abort(400, 'Not a JSON')
+    # check if key in dictionary is not allowed to be updated
+    for key_ignore in ignore_keys:
+        if key_ignore in to_update.keys():
+            del to_update[key_ignore]
+    if obj_city:
+        for key, value in to_update.items():
+            setattr(obj_city, key, value)
+        obj_city.save()
+        return jsonify(obj_city.to_dict())
+    else:
+        abort(404)
